be73567a6ffbe3f8ed398eb95bb5f7871f66bb4d
518651 <1725212799@qq.com>
2023-01-07 16:42:22 +0800

    Success Server Page Code Wirte

---
+++ KodPlay-CSGO-Client/Page/View/Server.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KodPlay_CSGO_Client
+{
+    /// <summary>
+    /// Server.xaml 的交互逻辑
+    /// </summary>
+    public partial class Server
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void Server_Model_Set(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetCsgoPath(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CommunityServerList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CloseContionlHC(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ActionConnect(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
