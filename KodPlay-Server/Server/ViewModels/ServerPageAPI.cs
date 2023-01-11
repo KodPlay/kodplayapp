4256829cb01a15213c237e689eff9d12d37f7c30
518651 <1725212799@qq.com>
2023-01-11 16:25:32 +0800

    Add New API Server Project

---
+++ KodPlay-Server/Server/ViewModels/ServerPageAPI.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KodPlay_Server.Server.ViewModels
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServerPageAPI : ControllerBase
+    {
+
+
+        
+    }
+}
+    
