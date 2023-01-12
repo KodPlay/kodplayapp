a57bdf3a9dec52e0d39cf8b0abae970c37b6904f
518651 <1725212799@qq.com>
2023-01-12 20:10:29 +0800

    Add DataBase Function

---
+++ KodPlay-Server/Database/Data/ServerInfo.cs
@@ -0,0 +1,19 @@
+﻿using Furion.DatabaseAccessor;
+
+namespace KodPlay_Server.Database
+{
+    public class ServerInfo : IEntity //不需要 Furion 为实体添加任何内置特性，选用 IEntity，无键实体选用 IEntityNotKey
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        /// 
+        public int? Sid { get; set; } //服务器编号
+
+        public string? IP { get; set; } //服务器IP地址
+        
+        public string? Port { get; set; } //服务器端口
+
+        // https://furion.baiqian.ltd/docs/entity/
+    }
+}
---
+++ KodPlay-Server/Database/Read/QueryServer.cs
@@ -0,0 +1,12 @@
+﻿namespace KodPlay_Server.Database.Read
+{
+    public class QueryServer
+    {
+
+        public int ServerCount()
+        {
+            var user = new ServerInfo { Sid = 27, IP = "47.4464",Port = "27015"};
+            return 1;
+        }
+    }
+}
